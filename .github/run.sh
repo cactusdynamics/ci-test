37625cb644857df551a9ab5b39bf34414bf4b691
Shuhao Wu <shuhao@shuhaowu.com>
2025-12-29 17:40:08 -0500

    Trying out woodpecker

--- .github/run.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-
-image_name=$GITHUB_WORKFLOW-ci-$GITHUB_REF_NAME
-
-set -xe
-
-docker build -f Dockerfile.test -t ${image_name} .
-docker run --rm $image_name /app/.github/run-in-container.sh
--- scripts/build-cpp.sh
+++ scripts/build-cpp.sh
@@ -2,7 +2,9 @@
 
 set -xe
 
+cd cpp
 cmake -B build
 cmake --build build -j $(nproc)
 build/citest
+
 
